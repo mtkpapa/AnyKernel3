d83ae5d1f184e308038469ca5032b00718f5fd74
mtkpapa <ebanashka10028@gmail.com>
2025-09-02 22:41:27 +0300

    anykernel.sh: Remove kernel.string

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Choynik Amogusson Kernel by @zainarbani & @huanan1
+kernel.string=
 do.devicecheck=1
 do.modules=0
 do.systemless=1
